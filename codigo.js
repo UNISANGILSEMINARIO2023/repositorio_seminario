d4c29ef9e1175312475fffc229381ed3bbc2772a
YILMERGAITAN <yexithperez06@gmail.com>
2023-03-05 21:24:55 -0500

    codigo actualizado

--- codigo.js
+++ codigo.js
@@ -1,3 +1,5 @@
-function restar (a,b){
-    Document.write (a-b)
-}
+num1 = float(input("¿Digite el primer número?"))
+num2 = float(input("¿Digite el segundo número?"))
+sumar= num1+num2
+
+print ("la suma es =",sumar)
