990079b02efb43ed74d575e36f393a0b92161894
YILMERGAITAN <yexithperez06@gmail.com>
2023-03-05 21:30:32 -0500

    codigo actualizado

--- codigo.js
+++
@@ -1,3 +0,0 @@
-function restar (a,b){
-    Document.write (a-b)
-}
---
+++ codigo.py
@@ -0,0 +1,6 @@
+num1 = float(input("¿Digite el primer número?"))
+num2 = float(input("¿Digite el segundo número?"))
+
+restar=num1-num2
+
+print ("la resta es =",restar)
